bb64dcd30ebc0c6d4ab99c013a11e6fe4f7d3e87
maisvendoo <maisvendoo@gmail.com>
2021-03-24 12:38:49 +0300

    Инициализация и проверка работы Vulkan и GLFW

---
+++ hello-glfw/include/main.h
@@ -0,0 +1,9 @@
+#ifndef     MAIN_H
+#define     MAIN_H
+
+#define     GLFW_INCLUDE_VULKAN
+#include    <GLFW/glfw3.h>
+
+#include    <iostream>
+
+#endif // MAIN_H
---
+++ hello-glfw/src/main.cpp
@@ -0,0 +1,25 @@
+#include    "main.h"
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
