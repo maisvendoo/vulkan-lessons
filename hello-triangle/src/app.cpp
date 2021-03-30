a89167c94fcbfc2aa133f203ad8d4da5ad30eb7e
maisvendoo <maisvendoo@gmail.com>
2021-03-30 19:55:46 +0300

    Обработка шейдеров - начало работы

--- hello-triangle/src/app.cpp
+++ hello-triangle/src/app.cpp
@@ -702,4 +702,27 @@ VkBool32 HelloTriangleApplication::debugCallback(VkDebugUtilsMessageSeverityFlag
     return VK_FALSE;
 }
 
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+std::vector<char> HelloTriangleApplication::readFile(const std::string &filename)
+{
+    std::ifstream file(filename, std::ios::ate |  std::ios::binary);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Failed to open file");
+    }
+
+    size_t fileSize = static_cast<size_t>(file.tellg());
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+
+    file.close();
+
+    return buffer;
+}
+
 
